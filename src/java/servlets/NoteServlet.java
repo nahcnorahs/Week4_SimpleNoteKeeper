7c7a26e0e23a27911f0470a10f76287aaeb4efbb
Sharon <>
2022-09-27 14:52:25 -0600

    Updated Contents in all areas (Not saving the contents)

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -5,6 +5,12 @@
  */
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -12,75 +18,61 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  *
  * @author Sharon
  */
 public class NoteServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NoteServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NoteServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        String noteEdit = request.getParameter("edit");
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");    
+        
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+            request.setAttribute("title", br.readLine());
+            request.setAttribute("content",br.readLine());
+            br.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File Not Found");
+        }
+        
+        if (noteEdit!=null){
+            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp")
+                    .forward(request,response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
+                    .forward(request,response);
+        }
+        
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+     
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path,false)));
+        pw.println(title);
+        pw.println(content);
+        pw.close();
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
+                .forward(request,response);
+        
+        /* Note note = new Note(title,content);
+        request.setAttribute("note",note); */
+        
+        
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
